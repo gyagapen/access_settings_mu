8b96ca0b1713ef72b45a30c85fd02aed6882f403
Yagapen Guillaume <guiyag@mcb.local>
2018-05-23 09:41:28 +0400

    first commit

---
+++ example/README.md
@@ -0,0 +1,8 @@
+# access_settings_menu_example
+
+Demonstrates how to use the access_settings_menu plugin.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ ios/Classes/AccessSettingsMenuPlugin.m
@@ -0,0 +1,20 @@
+#import "AccessSettingsMenuPlugin.h"
+
+@implementation AccessSettingsMenuPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"access_settings_menu"
+            binaryMessenger:[registrar messenger]];
+  AccessSettingsMenuPlugin* instance = [[AccessSettingsMenuPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
